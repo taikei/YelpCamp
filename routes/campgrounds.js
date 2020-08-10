ca4b762107d059ffb2a842f71423082b713aab7d
TAIKEI NAKANO <taikei0427@TAIKEInoMacBook-Air.local>
2020-08-10 11:36:54 +1000

    modify router

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -17,7 +17,7 @@ router.get('/', function(req, res){
        if(err){
            console.log(err)
        } else {
-        res.render('campgrounds/index', {campgrounds:data});
+        res.render('campgrounds/index', {campgrounds:data, page: 'campgrounds'});
        }
    });
 });
--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ router.get('/', function(req, res){
 
 // show register form 
 router.get('/register', function(req, res){
-    res.render('register');
+    res.render('register', {page: 'register'});
 });
 
 // handling signup logic
@@ -49,7 +49,7 @@ router.post('/register', (req, res) => {
 
 // show login form 
 router.get('/login', (req, res) => {
-    res.render('login');
+    res.render('login', {page: 'login'});
 }) 
 
 
